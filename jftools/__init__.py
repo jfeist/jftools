69ec07d9afb768bf192782ae42195eabfc6eb581
Johannes Feist <johannes.feist@gmail.com>
2016-11-26 12:48:34 +0100

    added __all__ in __init__.py

--- jftools/__init__.py
+++ jftools/__init__.py
@@ -2,6 +2,8 @@
 
 __version__ = '0.1'
 
+__all__ = ['shade_color','tic','toc','ipynbimport_install']
+
 from .shade_color import shade_color
 from .tictoc import tic, toc
 from .ipynbimport import install as ipynbimport_install
